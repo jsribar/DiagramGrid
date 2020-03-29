f9108be97f8611c4f1c309119b6b5e18ade914eb
jsribar <julijan.sribar@gmail.com>
2020-03-30 01:16:05 +0200

    Add project files.

---
+++ DiagramGrid/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace DiagramGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new DiagramGrid.Controls.Grid();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid1.Location = new System.Drawing.Point(12, 12);
+            this.grid1.MajorTicColor = System.Drawing.Color.LightSlateGray;
+            this.grid1.MajorXTicsCount = 3;
+            this.grid1.MajorYTicsCount = 2;
+            this.grid1.MinDelta = 10;
+            this.grid1.MinorTicColor = System.Drawing.Color.LightGray;
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(668, 440);
+            this.grid1.TabIndex = 0;
+            this.grid1.Text = "grid1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 464);
+            this.Controls.Add(this.grid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.Grid grid1;
+    }
+}
+
---
+++ DiagramGridControl/Grid.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiagramGrid.Controls
+{
+    public partial class Grid : Control, INotifyPropertyChanged
+    {
+        public Grid()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string PropertyName)
+        {
+            Invalidate();
+            Parent?.Invalidate(this.Bounds, true);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            DrawGrid(pe.Graphics);
+        }
+
+        private void DrawGrid(Graphics g)
+        {
+            using (Pen majorTicPen = new Pen(MajorTicColor))
+            using (Pen minorTicPen = new Pen(MinorTicColor))
+            {
+                int deltaYMajor = GetMajorYTicsDelta();
+                int nYMinor = deltaYMajor / minDelta;
+                int deltaY = nYMinor == 0 ? deltaYMajor : deltaYMajor / nYMinor;
+                int nY = (Height - 1) / deltaY;
+                for (int i = 0, y = Height - 1; i <= nY; ++i)
+                {
+                    if (nYMinor != 0 && i % nYMinor != 0)
+                        g.DrawLine(minorTicPen, 0, y, Width, y);
+                    else
+                        g.DrawLine(majorTicPen, 0, y, Width, y);
+                    y -= deltaY;
+                }
+
+                int deltaXMajor = GetMajorXTicsDelta();
+                int nXMinor = deltaXMajor / minDelta;
+                int deltaX = nXMinor == 0 ? deltaXMajor : deltaXMajor / nXMinor;
+                int nX = (Width - 1) / deltaX;
+                for (int i = 0, x = 0; i <= nX; ++i)
+                {
+                    if (nXMinor != 0 && i % nXMinor != 0)
+                        g.DrawLine(minorTicPen, x, 0, x, Height - 1);
+                    else
+                        g.DrawLine(majorTicPen, x, 0, x, Height - 1);
+                    x += deltaX;
+                }
+            }
+        }
+
+        private int GetMajorXTicsDelta()
+        {
+            return (Width - 1) / majorXTicsCount;
+        }
+
+        private int GetMajorYTicsDelta()
+        {
+            return (Height - 1) / majorYTicsCount;
+        }
+
+        [Category("Custom")]
+        [Browsable(true)]
+        [Description("Sets or gets the number of major tics count on x-axis")]
+        public int MajorXTicsCount
+        {
+            get => majorXTicsCount;
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentOutOfRangeException(nameof(MajorXTicsCount) + " must be > 0");
+                if (majorXTicsCount != value)
+                {
+                    majorXTicsCount = value;
+                    NotifyPropertyChanged(nameof(MajorXTicsCount));
+                }
+            }
+        }
+
+        [Category("Custom")]
+        [Browsable(true)]
+        [Description("Sets or gets the number of major tics count on y-axis")]
+        public int MajorYTicsCount
+        {
+            get => majorYTicsCount;
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentOutOfRangeException(nameof(MajorYTicsCount) + " must be > 0");
+                if (majorYTicsCount != value)
+                {
+                    majorYTicsCount = value;
+                    NotifyPropertyChanged(nameof(MajorYTicsCount));
+                }
+            }
+        }
+
+        [Category("Custom")]
+        [Browsable(true)]
+        [Description("Sets or gets the minimal distance between grid lines"), DefaultValue(20)]
+        public int MinDelta 
+        {
+            get => minDelta;
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentOutOfRangeException(nameof(MinDelta) + " must be > 0");
+                if (minDelta != value)
+                {
+                    minDelta = value;
+                    NotifyPropertyChanged(nameof(MinDelta));
+                }
+            }
+        }
+
+        [Category("Custom")]
+        [Browsable(true)]
+        [Description("Sets or gets the color of major tics line")]
+        public Color MajorTicColor
+        {
+            get => majorTicColor;
+            set
+            {
+                if (majorTicColor != value)
+                {
+                    majorTicColor = value;
+                    NotifyPropertyChanged(nameof(MajorTicColor));
+                }
+            }
+        }
+
+        [Category("Custom")]
+        [Browsable(true)]
+        [Description("Sets or gets the color of minor tics line")]
+        public Color MinorTicColor
+        {
+            get => minorTicColor;
+            set
+            {
+                if (minorTicColor != value)
+                {
+                    minorTicColor = value;
+                    NotifyPropertyChanged(nameof(MinorTicColor));
+                }
+            }
+        }
+
+        private int minDelta = 20;
+        private int majorXTicsCount = 3;
+        private int majorYTicsCount = 2;
+        private Color majorTicColor = Color.LightSlateGray;
+        private Color minorTicColor = Color.LightGray;
+    }
+}
